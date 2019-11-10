4df642adecf5f8f0f0d1ae4f9241797859c52451
Luke <lukele1607@gmail.com>
2019-11-10 22:05:01 +0700

    Delete QuoteItem

--- HydrotestCentral/ViewModels/QuoteItemsVM.cs
+++ HydrotestCentral/ViewModels/QuoteItemsVM.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Data.SQLite;
 using System.Windows.Input;
 using HydrotestCentral.Infrastructure;
 
@@ -42,7 +43,35 @@ namespace HydrotestCentral.ViewModels
             {
                 QuoteItems.Add (quoteItemVM);
             }
+
+            QuoteItems.CollectionChanged += QuoteItems_CollectionChanged;
         }
 
+        private void QuoteItems_CollectionChanged (object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            if (e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Remove)
+            {
+                var itemRemove = e.OldItems[0] as QuoteItemVM;
+
+                string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["connection_String"].ConnectionString;
+                using (SQLiteConnection connection = new SQLiteConnection ())
+                {
+                    connection.ConnectionString = connectionString;
+                    connection.Open ();
+                    using (SQLiteCommand command = new SQLiteCommand (connection))
+                    {
+                        command.CommandText =
+                            "DELETE FROM QTE_ITEMS where jobno = @jobno AND tab_index = @tab_index AND row_index= @row_index";
+
+                        command.Parameters.Add (new SQLiteParameter ("@jobno", itemRemove.JobNo));
+                        command.Parameters.Add (new SQLiteParameter ("@tab_index", itemRemove.TabIndex));
+                        command.Parameters.Add (new SQLiteParameter ("@row_index", itemRemove.RowIndex));
+
+
+                        command.ExecuteNonQuery ();
+                    }
+                }
+            }
+        }
     }
 }
